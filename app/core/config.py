8a846fec1873879ae50311626d2d7bc227cdf77a
digit987 <15653107+digit987@users.noreply.github.com>
2026-05-30 18:45:05 +0530

    Connect Qdrant Cloud

--- app/core/config.py
+++ app/core/config.py
@@ -7,10 +7,9 @@ load_dotenv()
 class Settings:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-    QDRANT_HOST = os.getenv("QDRANT_HOST")
-    QDRANT_PORT = int(os.getenv("QDRANT_PORT"))
+    QDRANT_URL = os.getenv("QDRANT_URL")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
     TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
-
     COLLECTION_NAME = "documents"
 
 
